980df140fae0d72b791178912a09358939e7b3f3
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2021-03-18 10:39:34 +0700

    Practical S1-SEM2

--- src/Pratical/S1_SEM2/Main.java
+++ src/Pratical/S1_SEM2/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         Car c= new Car();
-        Motor m= new Motor();
         System.out.print("Moi ban nhap ten xe oto: ");
         c.setName(sc.nextLine());
-        System.out.print("Moi ban nhap ten xe motor: ");
-        m.setName(sc.nextLine());
         System.out.println("Ten xe: "+c.getName());
         System.out.println("MaxSpeed: "+c.moveMax()+" km/h");
+        Motor m= new Motor();
+        System.out.print("Moi ban nhap ten xe motor: ");
+        m.setName(sc.nextLine());
         System.out.println("Ten xe: "+m.getName());
         System.out.println("MaxSpeed: "+m.moveMax()+" km/h");
     }
