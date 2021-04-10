a29cb3d15f67a1d4af1776759bd9c37845bb0b3b
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2021-04-10 11:18:49 +0700

    Practical adf2

--- src/pratical/adf2/Main.java
+++ src/pratical/adf2/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Menu Booking Reservation ARS");
         boolean run = true;
         ReservationSystem rs = new ReservationSystem();
         while (run){
+            System.out.println("Menu Booking Reservation ARS");
            System.out.println("1. Add reservation");
            System.out.println("2. Modify passenger records");
            System.out.println("3. Display passenger records");
